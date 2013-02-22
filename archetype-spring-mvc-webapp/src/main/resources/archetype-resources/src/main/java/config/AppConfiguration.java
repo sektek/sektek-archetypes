a8384fa28c41948e172bf89665a363190cc4fc2b
S. Edward Kelly <eddie@sektek.net>
2013-02-22 08:18:04 -0800

    Upgraded spring and moved View bean into a Java Config class.

---
+++ archetype-spring-mvc-webapp/src/main/resources/archetype-resources/src/main/java/config/AppConfiguration.java
@@ -0,0 +1,19 @@
+package ${package}.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class AppConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
